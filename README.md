5576a11e81e2dfdbb0a7f9a70b63af224ab5e63e
Onlyjossy <josephololade7@gmail.com>
2024-01-06 18:45:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+######
+This is my portfolio page,
+ navigating about me, 
+ what I do, 
+ and how to contact me!
+#####
---
+++ feet.js
@@ -0,0 +1,32 @@
+function more(){
+    document.getElementById('open').style.display ="grid";
+    document.getElementById('open1').style.display ="grid";
+    document.getElementById('open2').style.display ="grid";
+    document.getElementById('open3').style.display ="grid";
+    document.getElementById('open4').style.display ="grid";
+    document.getElementById('open5').style.display ="grid";
+    document.getElementById('more').style.display ="none";
+    document.getElementById('hehe').style.display ="inline";
+}
+function less(){
+    document.getElementById('more').style.display ="inline";
+    document.getElementById('open').style.display ="none";
+    document.getElementById('open1').style.display ="none";
+    document.getElementById('open2').style.display ="none";
+    document.getElementById('open3').style.display ="none";
+    document.getElementById('open4').style.display ="none";
+    document.getElementById('open5').style.display ="none";
+    document.getElementById('hehe').style.display ="none";
+}
+function clicks(){
+    document.getElementById('click').style.display ="none";
+    document.getElementById('click1').style.display ="grid";
+    document.getElementById('click2').style.display ="grid";
+    document.getElementById('haha').style.display ="inline"
+}
+function haha(){
+    document.getElementById('haha').style.display ="none";
+    document.getElementById('click').style.display ="inline";
+    document.getElementById('click1').style.display ="none";
+    document.getElementById('click2').style.display ="grid";
+}
